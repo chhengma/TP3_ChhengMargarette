9a1fea652de5c2c541a0f9968ddec20c62a8ab17
chhengma <chh.bee@gmail.com>
2023-10-23 19:04:45 -0400

    Ajout EventListener Zoom

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # TP3_ChhengMargarette
 https://github.com/chhengma/TP3_ChhengMargarette.git
 
-https://6535c9e67c315e549031b976--bespoke-shortbread-b8d17b.netlify.app/
+https://tp3-margarette-chheng.netlify.app/
--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
+//Event Listener: Zoom sur les images
+const zoomImages = document.querySelectorAll('.zoom-image');
+
+zoomImages.forEach(image => {
+	image.addEventListener('mouseover', function () {
+		image.style.transform = 'scale(1.1)';
+	});
+
+	image.addEventListener('mouseout', function () {
+		image.style.transform = 'scale(1)';
+	});
+});
+
 //Form Alert
 document.getElementById("monBouton").addEventListener("click", () => {
 	alert("Félicitations, vous faites désormais partie de notre cercle de crocheteurs !");
